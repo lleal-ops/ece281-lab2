c509f80477156c75eac17a77b023fcebd46b2209
lleal-ops <225130384+lleal-ops@users.noreply.github.com>
2026-03-05 12:27:04 -0700

    SUbmission

--- src/sevenseg_decoder.vhd
+++ src/sevenseg_decoder.vhd
@@ -41,7 +41,7 @@ architecture Behavioral of sevenseg_decoder is
 begin
 
 with i_hex select
-o_seg_n <=  "1000000" when "0000",
+o_seg_n <=  "0000001" when "0000",
         "1001111" when "0001",
         "0010010" when "0010",
         "0000110" when "0011",
